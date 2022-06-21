a799c695446a63780126b06c64ec470390c0a6ca
Diego Bonilla <drbv27@gmail.com>
2022-06-21 18:26:30 -0500

    returning to the basis

--- src/components/layout/FormRegistro.jsx
+++ src/components/layout/FormRegistro.jsx
@@ -15,49 +15,7 @@ const firestore = getFirestore(firebaseApp);
 
 const FormRegistro = ({usuario,setArrayTareas,arrayTareas}) => {
   const datain = data;
-/*   const datos = [
-    {
-      subproceso: "subproceso",
-      proceso: "proceso",
-      macroproceso: "macroproceso",
-    },
-    {
-      subproceso: "Asamblea General",
-      proceso: "Gobierno",
-      macroproceso: "Gerencia",
-    },
-    {
-      subproceso: "Junta Directiva",
-      proceso: "Gobierno",
-      macroproceso: "Gerencia",
-    },
-    {
-      subproceso: "Comite Asesor",
-      proceso: "Gobierno",
-      macroproceso: "Gerencia",
-    },
-    {
-      subproceso: "Dimensionar la Oportunidad",
-      proceso: "Descubrimiento de clientes",
-      macroproceso: "Innovación",
-    },
-    {
-      subproceso: "Diseño de Prototipo",
-      proceso: "Descubrimiento de clientes",
-      macroproceso: "Innovación",
-    },
-    {
-      subproceso: "Mantenimiento Preventivo",
-      proceso: "Manejo de equipos",
-      macroproceso: "Soporte",
-    },
 
-    {
-      subproceso: "Mantenimiento Correctivo",
-      proceso: "Manejo de equipos",
-      macroproceso: "Soporte",
-    },
-  ]; */
   const [subp, setSubp] = useState("");
   function cambio(e) {
     e.preventDefault();
@@ -71,93 +29,40 @@ const FormRegistro = ({usuario,setArrayTareas,arrayTareas}) => {
 
   
   return (
-    <ConteRegistro>
-      <ContBloq>
-        <ConSubBlo>
-          <LabelRegistro para="ingreso" texto="Fecha" />
-          <InputRegistro tipo="date" ident="formFecha" />
-        </ConSubBlo>
-        <ConSubBlo>
-          <LabelRegistro para="inicio" texto="Hora Inicio" />
-          <InputRegistro tipo="time" ident="formInicio" />
-        </ConSubBlo>
-      </ContBloq>
-      <ContBloq>
-        <ConSubBlo>
-          <LabelRegistro tipo="final" texto="Hora Final" />
-          <InputRegistro tipo="time" ident="formFinal" />
-        </ConSubBlo>
+    <>
+      
+      <form >
+        <input type="date" id="formFecha"/>
+        <input type="time" id="formInicio"/>
+        <input type="time" id="formFinal"/>
         
-{/*         <ConSubBlo>
-          <LabelRegistro para="subproces" texto="Subproceso" />
-          <InputRegistro
-            tipo="text"
-            ident="subproces"
-            placeh="Que actividad voy a realizar..."
-          />
-        </ConSubBlo> */}
-
-      </ContBloq>
-      <ContBloq>
-      {/*   <ConSubBlo>
-          <LabelRegistro para="proces" texto="Proceso" />
-          <InputRegistro
-            tipo="text"
-            ident="proces"
-            placeh="Que actividad voy a realizar..."
-          />
-        </ConSubBlo> */}
- {/*        <ConSubBlo>
-          <LabelRegistro para="macro" texto="Macroproceso" />
-          <InputRegistro
-            tipo="text"
-            ident="macro"
-            placeh="Que actividad voy a realizar..."
-          />
-        </ConSubBlo> */}
-      </ContBloq>
-      <ContBloq>
-        <ConSubBlo>
-          <LabelRegistro para="actividad" texto="Actividad" />
-          <TextareaRegistro
-            ident="formActividad"
-            placeh="Descripción de la actividad."
-            column="50"
-            rowst="3"
-          />
-        </ConSubBlo>
-      </ContBloq>
-
-      <label htmlFor="subprocesos">Escoja el subproceso:</label>
-      <input list="subprocesos" name="formSubproceso" id="formSubproceso" onChange={cambio}/>
-
-      <datalist id="subprocesos">
-      {datain.map((subproc,index) => (
+        <label htmlFor="subprocesos">Escoja el subproceso:</label>
+        <input list="subprocesos" name="formSubproceso" id="formSubproceso" onChange={cambio}/>
+        <datalist id="subprocesos">
+          {datain.map((subproc,index) => (
           <option key={index}>{subproc.subproceso}</option>
-        ))}
-      </datalist>
+          ))}
+        </datalist>
 
-      <input type="tipo" id="formProceso" size="tamaño" name="formProceso" readOnly value={subp===""
-        ?"":
-        datain
-          .filter((subpr) => subpr.subproceso.includes(subp))
-          .map((filtrado) => (
-            filtrado.proceso
-          ))}/>
-      <input type="tipo" id="formMacroproceso" size="tamaño" name="formMacroproceso" readOnly value={subp===""
-        ?"":
-        datain
-          .filter((subpr) => subpr.subproceso.includes(subp))
-          .map((filtrado) => (
-            filtrado.macroproceso
-          ))}/>
-
-
-     
-
-      <BotonRegiActiv tipo="submit" text="Registrar Actividad" />
-    </ConteRegistro>
-  );
+        <input type="tipo" id="formProceso" size="tamaño" name="formProceso" readOnly value={subp===""
+          ?"":
+          datain
+            .filter((subpr) => subpr.subproceso.includes(subp))
+            .map((filtrado) => (
+              filtrado.proceso
+            ))}/>
+        <input type="tipo" id="formMacroproceso" size="tamaño" name="formMacroproceso" readOnly value={subp===""
+          ?"":
+          datain
+            .filter((subpr) => subpr.subproceso.includes(subp))
+            .map((filtrado) => (
+              filtrado.macroproceso
+            ))}/>
+        <textarea placeholder="actividad" id="formActividad"/>
+        <input type="submit"/>
+      </form>
+    </>
+  )
 };
 
 export default FormRegistro;
